1379a3ca45c9ca2da316ade5a66d4816b8b1fcb0
Hasan <lmd744595@gmail.com>
2024-10-11 13:58:49 +0600

    update main.js

--- main.js
+++ main.js
@@ -1,1 +1,14 @@
-console.log('Hello World!');
+var x =document.querySelector("#icon");
+var y =document.querySelector("#upper");
+
+x.addEventListener('click',function () {
+if (y.style.display==='none'||y.style.display==='') {
+    
+y.style.display='block';
+    
+  } else {
+y.style.display='none';
+  }
+});
+
+
